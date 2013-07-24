171caec32000a0c3954fea5129ca904bf476ae37
Joel Wehr <joel.wehr@gmail.com>
2013-07-24 12:16:37 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,9 +15,9 @@
 	var defaultKey		= '2L3QzepoFohXmlpIbIYbaJqCou85saDcbbsBApYOa7ukXeTs', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1990153056!grillTemp!probeTemp], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Codename: jTherm-Pro', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
